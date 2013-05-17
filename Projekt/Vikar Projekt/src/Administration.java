29944108d8c599aab4906666a92c7d44fd9eafce
nicaa <nica1408@gmail.com>
2013-05-17 14:48:40 +0200

    Update Administration.java

--- Projekt/Vikar Projekt/src/Administration.java
+++ Projekt/Vikar Projekt/src/Administration.java
@@ -3,7 +3,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
+// hej Rasmus
 public class Administration extends DatabaseCon {
 
 	public String[] login() throws SQLException {
